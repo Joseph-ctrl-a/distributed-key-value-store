e5ff771b175579c2af47c5d61ceb037093bc43a0
Joseph <JosephOgbo18@outlook.com>
2026-04-29 14:04:01 +0100

    create connections on node

--- pkg/raft/node.go
+++ pkg/raft/node.go
@@ -1,10 +1,13 @@
 package raft
 
 import (
+	"Distributed_Key_Value_Store/pkg/transport"
 	"Distributed_Key_Value_Store/pkg/wal"
 	"flag"
 	"strings"
 	"time"
+
+	"google.golang.org/grpc"
 )
 
 type Node struct {
@@ -15,11 +18,12 @@ type Node struct {
 	votedFor      string
 	peers         []string
 	log           *wal.Wal
-	ElectionTimer time.Timer
+	electionTimer time.Timer
+	clients       map[string]transport.RaftClient
 }
 
 // NewNode defines how a Node should look
-func NewNode(wal *wal.Wal) *Node {
+func NewNode(wal *wal.Wal) (*Node, error) {
 	id := flag.String("id", "", "the node's ip:port")
 	peers := flag.String("peers", "", "comma separated list of peer addresses")
 
@@ -27,12 +31,27 @@ func NewNode(wal *wal.Wal) *Node {
 
 	peerList := strings.Split(*peers, ",")
 
-	node := &Node{id: *id, peers: peerList, role: "follower", currentLeader: "", votedFor: "", log: wal, ElectionTimer: *time.NewTimer(time.Millisecond * 150)}
+	node := &Node{id: *id, peers: peerList, role: "follower", currentLeader: "", votedFor: "", log: wal, electionTimer: *time.NewTimer(time.Millisecond * 150)}
 
+	err := node.createConnections()
+	if err != nil {
+		return nil, err
+	}
 	node.startElectionTimer()
-	return node
+	return node, nil
 }
 
+func (n *Node) createConnections() error {
+	for _, peerAddress := range n.peers {
+		connection, err := grpc.NewClient(peerAddress)
+
+		if err != nil {
+			return err
+		}
+		n.clients[peerAddress] = transport.NewRaftClient(connection)
+	}
+	return nil
+}
 func (n *Node) startElection() {
 	defer n.startElectionTimer()
 
@@ -41,12 +60,12 @@ func (n *Node) startElectionTimer() {
 	go func() {
 		defer n.resetElectionTimer()
 
-		<-n.ElectionTimer.C
+		<-n.electionTimer.C
 
 		n.startElection()
 	}()
 }
 
 func (n *Node) resetElectionTimer() {
-	defer n.ElectionTimer.Reset(time.Millisecond * 150)
+	defer n.electionTimer.Reset(time.Millisecond * 150)
 }
